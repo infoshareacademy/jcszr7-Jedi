a5212a5f3a69d9423126f85cd5ee8532940c7b6b
Albert Moczadło <albertmoczadlo@gmail.com>
2022-10-13 09:23:29 +0200

    Revert "remove tes"
    This reverts commit 01bd9a9bc038cf310ac3cb69971e9296b4d0eac2.

---
+++ JediApp/JediApp.Services/Services/MenuRoleUserService.cs
@@ -0,0 +1,44 @@
+﻿using JediApp.Database.Domain;
+using JediApp.Services.Helpers;
+
+namespace JediApp.Services.Services
+{
+    public class MenuRoleUserService
+    {
+        private readonly IUserService _userService;
+
+        public MenuRoleUserService(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public void RegisterUser()
+        {
+            var user = new User();
+            user.Role = "user";
+
+            Console.WriteLine("Enter new login");
+            user.Login = MenuOptionsHelper.CheckString(Console.ReadLine());
+
+            Console.WriteLine("Enter password");
+            user.Password = MenuOptionsHelper.CheckString(Console.ReadLine());
+
+            var registeredUser = _userService.AddUser(user);
+            if (registeredUser != null)
+            {
+                Console.WriteLine($"Login {user.Login} has been created.");
+            }
+            else
+            {
+                if (!_userService.IfLoginIsUnique(user))
+                {
+                    Console.WriteLine($"Login {user.Login} has already been used. Try another one...");
+                }
+                else
+                {
+                    Console.WriteLine($"Login {user.Login} has NOT been created.");
+                }
+            }
+        }
+    }
+}
