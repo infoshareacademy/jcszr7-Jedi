ddc2ab772b19e9bc2ccd23901a2788db048c2f23
Albert <albertmoczadlo@gmail.com>
2022-11-29 10:20:17 +0100

    deleted files

--- JediApp/JediApp.Database/Domain/User.cs
+++ JediApp/JediApp.Database/Domain/User.cs
@@ -7,16 +7,8 @@ namespace JediApp.Database.Domain
     public class User : IdentityUser
 
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
-        //public List<WalletPosition> WalletStatus { get; set; }
-        public Wallet Wallet { get; set; }
-        public UserRole Role { get; set; }
-       
-        public User()
-        {
-            this.Wallet = new Wallet();
-        }
+        public  string Name { get; set; }
+        public string SurrName { get; set; }
     }
 
 
