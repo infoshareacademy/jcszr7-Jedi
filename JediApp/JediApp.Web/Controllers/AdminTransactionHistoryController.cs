87b9a50f57b15a2eb8316ce9af41f12fa0e54fd4
Kasia Rogalska <kgrono@gmail.com>
2022-12-16 22:12:08 +0100

    Adding MENU + empty controllers for menu links

---
+++ JediApp/JediApp.Web/Controllers/AdminTransactionHistoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JediApp.Web.Controllers
+{
+    public class AdminTransactionHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["activePage"] = "AdminTransactionHistory";
+
+            return View();
+        }
+    }
+}
