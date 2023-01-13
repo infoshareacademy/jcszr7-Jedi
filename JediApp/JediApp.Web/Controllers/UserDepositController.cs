c690ff830fcd676d69e62b127283e2f5c1a9efb6
Kasia Rogalska <kgrono@gmail.com>
2023-01-13 11:10:21 +0100

    Poprawka Menu - user/admin role

---
+++ JediApp/JediApp.Web/Controllers/UserDepositController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JediApp.Web.Controllers
+{
+    public class UserDepositController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["activePage"] = "UserDeposit";
+            return View();
+        }
+    }
+}
---
+++ JediApp/JediApp.Web/Controllers/UserWithdrawalController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JediApp.Web.Controllers
+{
+    public class UserWithdrawalController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["activePage"] = "UserWithdrawal";
+            return View();
+        }
+    }
+}
