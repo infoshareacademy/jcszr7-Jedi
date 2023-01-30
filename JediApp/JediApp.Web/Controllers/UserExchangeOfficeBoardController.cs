af496e797e1483f6437bbf1282523c1721af4461
Mirosław Pizun <miropoczta8@gmail.com>
2023-01-30 11:54:29 +0100

    user ExchangeOfficeBoard add details/charts

--- JediApp/JediApp.Web/Controllers/UserExchangeOfficeBoardController.cs
+++ JediApp/JediApp.Web/Controllers/UserExchangeOfficeBoardController.cs
@@ -1,14 +1,204 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JediApp.Database.Domain;
+using JediApp.Database.Repositories;
+using JediApp.Services.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JediApp.Web.Controllers
 {
     public class UserExchangeOfficeBoardController : Controller
     {
-        public IActionResult Index()
-        {
-            ViewData["activePage"] = "UserExchange";
+        private readonly IExchangeOfficeBoardService _exchangeOfficeBoardService;
+        private readonly INbpJsonService _nbpJsonService;
+        private readonly IExchangeOfficeService _exchangeOfficeService;
 
+
+        public UserExchangeOfficeBoardController(IExchangeOfficeBoardService exchangeOfficeBoardService, INbpJsonService nbpJsonService, IExchangeOfficeService exchangeOfficeService)
+        {
+            _exchangeOfficeBoardService = exchangeOfficeBoardService;
+            _nbpJsonService = nbpJsonService;
+            _exchangeOfficeService = exchangeOfficeService;
+        }
+
+        //public ExchangeOfficeBoardController()
+        //{
+        //    _exchangeOfficeBoardService = new ExchangeOfficeBoardService(new ExchangeOfficeBoardRepository());
+        //}
+
+        // GET: ExchangeOfficeBoardController
+        public ActionResult Index()
+        {
+            ViewData["activePage"] = "AdminExchange";
+
+            var model = _exchangeOfficeBoardService.GetAllCurrencies();
+
+            return View(model);
+        }
+
+        // GET: ExchangeOfficeBoardController/Details/5
+        public ActionResult Details(Guid id)
+        {
+            var currency = _exchangeOfficeBoardService.GetCurrencyById(id);
+            return View(currency);
+        }
+
+        // GET: ExchangeOfficeBoardController/Create
+        public ActionResult Create()
+        {
             return View();
         }
+
+        // POST: ExchangeOfficeBoardController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Currency newCurrency)
+        {
+            _exchangeOfficeBoardService.AddCurrency(newCurrency);
+
+            return RedirectToAction(nameof(Index));
+
+            if (!ModelState.IsValid)
+            {
+                return View(newCurrency);
+            }
+            try
+            {
+                _exchangeOfficeBoardService.AddCurrency(newCurrency);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ExchangeOfficeBoardController/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            var currency = _exchangeOfficeBoardService.GetCurrencyById(id);
+            if (currency != null)
+            {
+                return View(currency);
+            }
+            else
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            //return View();
+        }
+
+        //// POST: ExchangeOfficeBoardController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Guid id, Currency currency)
+        {
+            //try
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+
+            _exchangeOfficeBoardService.UpdateCurrency(id, currency);
+
+            return RedirectToAction(nameof(Index));
+
+            if (!ModelState.IsValid)
+            {
+                return View(currency);
+            }
+
+            try
+            {
+                _exchangeOfficeBoardService.UpdateCurrency(id, currency);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //// GET: ExchangeOfficeBoardController/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            var currency = _exchangeOfficeBoardService.GetCurrencyById(id);
+
+            return View(currency);
+        }
+
+        // POST: ExchangeOfficeBoardController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id, IFormCollection collection)
+        {
+            try
+            {
+                var currencyToDelete = _exchangeOfficeBoardService.GetCurrencyById(id);
+                _exchangeOfficeBoardService.DeleteCurrencyByShortName(currencyToDelete.ShortName);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: ExchangeOfficeBoardController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCurrencyFromNbpApi()
+        {
+
+            List<Currency> nbpCurrencies = _nbpJsonService.GetAllCurrencies();
+
+            foreach (var currency in nbpCurrencies)
+            {
+                _exchangeOfficeBoardService.AddCurrency(new Currency()
+                {
+                    Name = currency.Name,
+                    ShortName = currency.ShortName,
+                    Country = currency.Country,
+                    BuyAt = currency.BuyAt,
+                    SellAt = currency.SellAt
+                });
+            }
+
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Markup()
+        {
+
+            var currencies = _exchangeOfficeBoardService.GetAllCurrencies();
+
+            foreach (var currency in currencies)
+            {
+
+                var markup = _exchangeOfficeService.GetAllExchangeOffices().FirstOrDefault().Markup;
+                //spread 5%
+                //currency.BuyAt *= (decimal)1.025;
+                //currency.SellAt *= (decimal)0.975;                
+                currency.BuyAt *= 1 + (decimal)markup / 200;
+                currency.SellAt *= 1 - (decimal)markup / 200;
+                _exchangeOfficeBoardService.UpdateCurrency(currency.Id, currency);
+
+            }
+
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
     }
 }
