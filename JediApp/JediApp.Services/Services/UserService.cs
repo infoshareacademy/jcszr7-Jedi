9da32c0c7adfe1edd830b6e49e16ad01c0b68a0f
Albert <albertmoczadlo@gmail.com>
2023-03-05 13:56:10 +0100

    Added tests.

--- JediApp/JediApp.Services/Services/UserService.cs
+++ JediApp/JediApp.Services/Services/UserService.cs
@@ -10,9 +10,9 @@ namespace JediApp.Services.Services
     {
         private readonly IUserRepository _userRepository;
 
-        public UserService(IServiceProvider userRepository)
+        public UserService(IUserRepository userRepository)
         {
-            _userRepository = ActivatorUtilities.GetServiceOrCreateInstance<IUserRepository>(userRepository);
+            _userRepository = userRepository;
         }
 
         public async Task<User> UpdateUser(User user)
@@ -26,51 +26,6 @@ namespace JediApp.Services.Services
 
             return users;
         }
-
-        //public void DeleteUser(string id)
-        //{
-        //    var user = _userRepository.GetUserById(id);
-        //}
-        //public User GetUserById(Guid id)
-        //{
-        //    return _userRepository.GetUserById(id);
-        //}
-
-        //public User GetUserByLogin(string login)
-        //{
-        //    return _userRepository.GetUserByLogin(login);
-        //}
-
-        //public List<User> GetAllUsers()
-        //{
-
-        //    return _userRepository.GetAllUsers();
-        //}
-
-        //public User AddUser(User user)
-        //{
-        //    if (IfLoginIsUnique(user))
-        //    {
-        //      return _userRepository.AddUser(user);
-        //    }
-
-        //    return null;
-        //}
-
-        //public List<User> BrowseUsers(string query)
-        //{
-        //    return _userRepository.BrowseUsers(query);
-        //}
-
-        //public bool IfLoginIsUnique(User user)
-        //{
-        //    foreach (var _user in GetAllUsers())
-        //    {
-        //        if(user.Login.Equals(_user.Login))
-        //            return false;
-        //    }
-        //    return true;
-        //}
 
     }
 }
